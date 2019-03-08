1b68b79a80c50b660e4f49ac52b2e7e3cba6992f
quanghuy1242 <katygaga124@gmail.com>
2019-03-08 17:48:59 +0700

    Build App

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Blog
 Blog
+
+# Build 
+`ng build --prod --output-path docs --base-href "https://quanghuy1242.github.io/blog/"`
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBHb0GaJ-TEgHZ3iwJrMLQ0_wwXPzX_2g4",
+    authDomain: "blog-1f85f.firebaseapp.com",
+    databaseURL: "https://blog-1f85f.firebaseio.com",
+    projectId: "blog-1f85f",
+    storageBucket: "blog-1f85f.appspot.com",
+    messagingSenderId: "498507894826"
+  }
 };
