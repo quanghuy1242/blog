0fa847badaf92ec917078f210b1d15730ee00cc3
quanghuy1242 <katygaga124@gmail.com>
2019-04-12 20:30:47 +0700

    sorry i am wrong

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     scrollPositionRestoration: 'enabled',
-    // preloadingStrategy: PreloadAllModules
+    preloadingStrategy: PreloadAllModules
   })],
   exports: [RouterModule]
 })
